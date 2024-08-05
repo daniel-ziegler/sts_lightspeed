bbd5d935ab772205cfbc145d48c00a90100fddb6
Daniel Ziegler <ziedaniel1@gmail.com>
2024-08-04 20:46:30 -0700

    runnable from python

---
+++ run.py
@@ -0,0 +1,15 @@
+import slaythespire as sts
+
+gc = sts.GameContext(sts.CharacterClass.IRONCLAD, 777, 0)
+print(gc.map)
+agent = sts.Agent()
+while gc.outcome == sts.GameOutcome.UNDECIDED:
+    if gc.screen_state == sts.ScreenState.BATTLE:
+        agent.playout_battle(gc) 
+        print(gc)
+    else:
+        actions = sts.GameAction.getAllActionsInState(gc)
+        action = actions[0]
+        print(action.getDesc(gc))
+        action.execute(gc)
+
