99aab68efae90699d5df1ca255720fbc9bd0c990
Daniel Ziegler <ziedaniel1@gmail.com>
2024-08-04 20:48:32 -0700

    run faster

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import slaythespire as sts
 gc = sts.GameContext(sts.CharacterClass.IRONCLAD, 777, 0)
 print(gc.map)
 agent = sts.Agent()
+agent.simulation_count_base = 5000
+
 while gc.outcome == sts.GameOutcome.UNDECIDED:
     if gc.screen_state == sts.ScreenState.BATTLE:
         agent.playout_battle(gc) 
