812c7362e7ded43478e14db43c9fd364b461f980
Daniel Ziegler <ziedaniel1@gmail.com>
2024-08-04 21:02:33 -0700

    obs

--- run.py
+++ run.py
@@ -1,17 +1,25 @@
+import sys
+
 import slaythespire as sts
 
-gc = sts.GameContext(sts.CharacterClass.IRONCLAD, 777, 0)
+seed = len(sys.argv) > 1 and int(sys.argv[1]) or 0
+gc = sts.GameContext(sts.CharacterClass.IRONCLAD, seed, 0)
 print(gc.map)
+
+nni = sts.getNNInterface()
+print("MAXIMUMS:", nni.getObservationMaximums())
+
 agent = sts.Agent()
 agent.simulation_count_base = 5000
 
 while gc.outcome == sts.GameOutcome.UNDECIDED:
     if gc.screen_state == sts.ScreenState.BATTLE:
         agent.playout_battle(gc) 
-        print(gc)
+        print(nni.getObservation(gc))
     else:
         actions = sts.GameAction.getAllActionsInState(gc)
         action = actions[0]
         print(action.getDesc(gc))
         action.execute(gc)
 
+print(gc.outcome)
