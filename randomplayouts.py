086c22d52cb6d0446180811c5bc83efc27753232
Daniel Ziegler <ziedaniel1@gmail.com>
2024-08-22 07:46:10 -0700

    dicts

--- randomplayouts.py
+++ randomplayouts.py
@@ -2,10 +2,12 @@
 import sys
 import random
 from enum import IntEnum, auto
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 import pickle
+from tqdm.auto import tqdm
 
 import numpy as np
+import pandas as pd
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -13,7 +15,17 @@ import torch.nn.functional as F
 import slaythespire as sts
 
 # %%
-# todo pyarrow everything
+def flatten_dict(d, parent_key='', sep='.'):
+    items = []
+    for k, v in d.items():
+        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+        if isinstance(v, dict):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+# %%
 @dataclass
 class Choice:
     obs: sts.NNRepresentation
@@ -22,13 +34,25 @@ class Choice:
     actions: list[sts.GameAction]
     choice: int  # idx in actions
 
+    def as_dict(self):
+        return dict(
+            obs=self.obs.as_dict(),
+            actions=np.array([a.bits for a in self.actions], dtype=np.int32),
+            cards_offered=dict(
+                # TODO preserve 2d structure, doesn't work for pyarrow
+                cards=np.concatenate([o.cards for o in self.cards_offered], axis=0) if self.cards_offered else np.array([], dtype=np.uint16),
+                upgrades=np.concatenate([o.upgrades for o in self.cards_offered], axis=0) if self.cards_offered else np.array([], dtype=np.int32),
+            ),
+            paths_offered=np.array(self.paths_offered, dtype=np.int32),
+        )
+
 # %%
 
-def random_playout(seed: int):
+def random_playout(seed: int, verbose: bool = False):
     gc = sts.GameContext(sts.CharacterClass.IRONCLAD, seed, 0)
 
     agent = sts.Agent()
-    agent.simulation_count_base = 5000
+    agent.simulation_count_base = 10000
     choices: list[Choice] = []
 
     while gc.outcome == sts.GameOutcome.UNDECIDED:
@@ -54,24 +78,42 @@ def random_playout(seed: int):
             elif gc.screen_state == sts.ScreenState.EVENT_SCREEN:
                 # TODO neow, events
                 chosen = random.choice(actions)
+            elif gc.screen_state == sts.ScreenState.REST_ROOM:
+                if gc.cur_hp < 30:
+                    chosen = actions[0]
             elif gc.screen_state == sts.ScreenState.MAP_SCREEN:
                 def xy_to_roomid(x, y):
-                    roomids = [i for i in range(len(obs.map.xs)) if obs.map.xs[i] == x and obs.map.ys[i] == y]
-                    if len(roomids) != 1:
-                        print(x, y, obs.map.xs, obs.map.edge_starts)
-                    roomid, = roomids
+                    roomid, = [i for i in range(len(obs.map.xs)) if obs.map.xs[i] == x and obs.map.ys[i] == y]
                     return roomid
                 paths_offered = [xy_to_roomid(a.idx1, gc.cur_map_node_y+1) for a in actions]
             if chosen is None:
                 choice = random.randrange(len(actions))
                 choices.append(Choice(obs, cards_offered=cards_offered, paths_offered=paths_offered, actions=actions, choice=choice))
                 chosen = actions[choice]
-            print(chosen.getDesc(gc))
+            if verbose:
+                print(chosen.getDesc(gc))
             chosen.execute(gc)
 
+    print(gc.outcome, gc.floor_num)
     return (choices, gc.outcome)
 # %%
-choices, outcome = random_playout(777)
-print(outcome)
+def random_playout_data(seed: int):
+    choices, outcome = random_playout(777)
+    df = pd.DataFrame([flatten_dict(c.as_dict()) for c in choices])
+    df["outcome"] = {sts.GameOutcome.PLAYER_LOSS: 0, sts.GameOutcome.PLAYER_VICTORY: 1}[outcome]
+    df["seed"] = seed
+    return df
 
+# %%
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+num_threads = 8
+num_playouts = 320
+
+with ThreadPoolExecutor(max_workers=num_threads) as executor:
+    futures = [executor.submit(random_playout_data, s) for s in range(num_playouts)]
+    df = pd.concat([future.result() for future in tqdm(as_completed(futures), total=num_playouts)])
+
+df.to_parquet("rollouts.parquet", engine="pyarrow")
 # %%
